89f4bf1d5f382c93adc3406e2682d7c932b5d7f6
DESKTOP-L44RQMV\Kacper Daraż 4TI <kacperdaraz07@gmail.com>
2024-09-30 11:24:22 +0200

    dodany skrypt

--- Filmy/src/App.js
+++ Filmy/src/App.js
@@ -1,22 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import { useState } from 'react';
 function App() {
+
+
+  const [tytul, setTytul] = useState("");
+  const [rodzaj, setRodzaj] = useState("");
+
+  const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(`Tytuł: ${tytul}, Rodzaj: ${rodzaj}`);
+  }
   return (
       <div className="App">
       <form>
         <div class="mb-3">
-          <div id="film" class="form-label">Tytuł filmu</div>
-          <input type="text" class="form-control" id="film"/>
+          <div id="film" className="form-label">Tytuł filmu</div>
+          <input type="text" className="form-control" id="film"/>
         </div>
-        <div id="Wybierz" class="from-label">Wybierz rodzaj filmu </div>
-        <select id="Select" class="form-control">
+        <div id="Wybierz" className="from-label">Wybierz rodzaj filmu </div>
+        <select id="Select" className="form-control">
           <option></option>
           <option>Komedia</option>
           <option>Sensacyjny</option>
           <option>Horror</option>
           <option>Obyczajowy</option>
         </select>
-        <div><button type="submit" class="btn btn-primary">Dodaj</button></div>
+        <div><button type="submit" className="btn btn-primary">Dodaj</button></div>
       </form>
     </div>
   );
